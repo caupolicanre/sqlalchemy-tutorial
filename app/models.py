c720db8bc4856fe3da54830f7aa9cdc5022a4d7a
caupolicanre <caupolicanre@gmail.com>
2024-09-24 15:08:43 -0300

    chore: Rename utis db folder to funcs

---
+++ app/models.py
@@ -0,0 +1,46 @@
+import datetime
+
+from typing import List, Optional
+
+from sqlalchemy.orm import DeclarativeBase, Mapped
+from sqlalchemy.orm import relationship, mapped_column
+from sqlalchemy import (Column, Integer, String, ForeignKey, Float, DateTime)
+
+from db.funcs.db_connection import get_db_connection
+
+
+
+engine = get_db_connection()
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "user_account"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]] = mapped_column(String(80))
+
+    addresses: Mapped[List["Address"]] = relationship(back_populates="user")
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, username={self.username!r}, fullname={self.fullname!r})"
+
+
+class Address(Base):
+    __tablename__ = "address"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email_address: Mapped[str] = mapped_column(String(100), nullable=False)
+    user_id = mapped_column(ForeignKey("user_account.id"))
+
+    user: Mapped[User] = relationship(back_populates="addresses")
+
+    def __repr__(self) -> str:
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+
+
+
+Base.metadata.create_all(engine)
